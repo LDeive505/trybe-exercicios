15c9669aac13e3867fc4d95e3b71b884a45b67b1
LDeive505 <luisdaviab@gmail.com>
2022-02-03 20:39:05 -0300

    Adição dos exércicios 1 à 6 do dia 1 do bloco 4

---
+++ Fundamentos/Bloco-4-Introducao-a-javascrip-e-logica-de-programacaoo/Dia-1-javascript-primeiros-passos/exercicios/exercicio-3.js
@@ -0,0 +1,11 @@
+const a = 37;
+const b= 127;
+const c = 34;
+
+if(a>b && a>c){
+    console.log("Maior Número: " + a);
+}else if(b>a && b>c){
+    console.log("Maior Número: " + b);
+}else if(c>a && c>b){
+    console.log("Maior Número: " + c);
+}
