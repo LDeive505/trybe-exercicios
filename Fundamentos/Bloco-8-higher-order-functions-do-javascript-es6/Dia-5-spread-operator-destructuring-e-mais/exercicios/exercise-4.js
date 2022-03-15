454fa3091cc7d0920a7e501904200e2381640331
LDeive505 <luisdaviab@gmail.com>
2022-03-15 20:06:32 -0300

    adição dos exercícios 1 ao 7, dia 5, bloco 8

---
+++ Fundamentos/Bloco-8-higher-order-functions-do-javascript-es6/Dia-5-spread-operator-destructuring-e-mais/exercicios/exercise-4.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    name: "Nicole",
+    bornIn: 1992,
+    nationality: "Australian",
+  },
+  {
+    name: "Harry",
+    bornIn: 2008,
+    nationality: "Australian",
+  },
+  {
+    name: "Toby",
+    bornIn: 1901,
+    nationality: "Australian",
+  },
+  {
+    name: "Frida",
+    bornIn: 1960,
+    nationality: "Dannish",
+  },
+  {
+    name: "Fernando",
+    bornIn: 2001,
+    nationality: "Brazilian",
+  },
+];
+
+// escreva filterPeople abaixo
+
+const filterPeople = (people) =>
+  people.filter(
+    ({ bornIn, nationality }) => nationality === "Australian" && bornIn < 2001 && bornIn > 1900
+  );
+
+console.log(filterPeople(people));
