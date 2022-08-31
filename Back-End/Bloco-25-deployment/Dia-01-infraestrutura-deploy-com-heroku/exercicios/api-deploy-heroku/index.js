7a187cd467c50351398b9440f84948c44d2fdb3f
LDeive505 <luisdaviab@gmail.com>
2022-08-31 20:34:40 -0300

    start deploy heroku

--- Back-End/Bloco-25-deployment/Dia-01-infraestrutura-deploy-com-heroku/exercicios/api-deploy-heroku/index.js
+++ Back-End/Bloco-25-deployment/Dia-01-infraestrutura-deploy-com-heroku/exercicios/api-deploy-heroku/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Está vivo!!!');
+app.get('/', (req, res) => {
+  res.send('Está VIVO!!!');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Ouvindo na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
