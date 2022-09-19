b7483c69fe49549c50c0ec77274059eb67f408b8
LDeive505 <luisdaviab@gmail.com>
2022-09-19 18:55:26 -0300

    feat:: exercicios do dia

---
+++ Back-End/Secao-9-programacao-orientada-a-objetos-e-solid/Dia-03-polimorfismo/exercicios/Person.ts
@@ -0,0 +1,38 @@
+export default abstract class Person {
+  constructor(private _name: string, private _birthDate: Date) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this.validateName(value);
+    this._name = value;
+  }
+
+  get birthDate(): Date {
+    return this._birthDate;
+  }
+
+  set birthDate(value: Date) {
+    this.validateBirthDate(value);
+    this._birthDate = value;
+  }
+
+  static getAge(value: Date): number {
+    const diff = Math.abs(new Date().getTime() - value.getTime());
+    const yearMs = 31_536_000_000; // 1 ano = 31536000000 milissegundos
+    return Math.floor(diff / yearMs);
+  }
+
+  private validateName(value: string): void {
+    if (value.length < 3) throw new Error('O nome deve conter no mínimo 3 caracteres.');
+  }
+
+  private validateBirthDate(value: Date): void {
+    if (value.getTime() > new Date().getTime()) {
+      throw new Error('A pessoa não pode ser um viajante do tempo.');
+    } 
+    if (Person.getAge(value) > 120) throw new Error('A pessoa provavelmente está morta.');
+  }
+}
